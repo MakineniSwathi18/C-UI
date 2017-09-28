0be4b13bf76cec6118143b7a7e333eed0c9fb0f1
Your Name <ranjith.sakala@gmail.com>
2017-09-28 17:07:10 -0500

    test commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ranjith Kumar Sakala\Documents\testing.mdf;Integrated Security=True;Connect Timeout=30");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into Table11 values ('"+textBox1.Text+ "','" + textBox2.Text + "','" + textBox3.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            disp_data();
+            MessageBox.Show("Record Inserted successully");
+        }
+
+        public void disp_data()
+        {
+            
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Table11";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+            }
+
+      
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            disp_data();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from Table11 where fname= '"+textBox1.Text+"'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            disp_data();
+            MessageBox.Show("Record deleted successully");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update Table11 set fname='" + textBox2.Text + "' where fname ='" + textBox1.Text + "' ";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            disp_data();
+            MessageBox.Show("Record Updated successully");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            disp_data();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
